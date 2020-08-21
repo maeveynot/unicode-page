ac857be58476aadb162edf8f770eb5114a951d6f
Maeve Andrews <git@maeveandrews.com>
2020-08-21 14:19:22 -0400

    Leave facade up until user changes visibility herself

--- script.js
+++ script.js
@@ -1,45 +1,48 @@
 const initialScript = "sE";
+const initialToggle = document.getElementById(initialScript);
 const toggles = Array.from(document.getElementsByClassName("box"));
 
-const initToggles = () => {
-  if (!toggles.some(box => box.checked)) {
-    document.getElementById(initialScript).checked = true;
-  }
-};
-
-const updateVisibility = () => {
+const updateVisibility = target => {
   const visible = new Set(toggles.flatMap(box =>
     box.checked ? Array.from(document.querySelectorAll(".seg." + box.value)) : []
   ));
   document.querySelectorAll(".seg").forEach(node => {
     node.style.display = visible.has(node) ? "block" : "none";
   });
-};
-
-const setAllToggles = (predicate) => {
-  toggles.forEach(box => {
-    box.checked = predicate(box);
-  });
-  updateVisibility();
+  if (initialToggle.indeterminate || target == initialToggle) {
+    initialToggle.indeterminate = false;
+    document.getElementById("facade").style.display = "none";
+  }
 };
 
 document.getElementById("show-init").addEventListener("click", event => {
-  setAllToggles(box => box.id == initialScript);
+  document.getElementById("facade").style.display = "block";
+  toggles.forEach(box => {
+    box.checked = false;
+    if (box.id == initialScript) {
+      box.indeterminate = true;
+    }
+  });
+  document.querySelectorAll(".seg").forEach(node => {
+    node.style.display = "none";
+  });
 });
 document.getElementById("show-rand").addEventListener("click", event => {
-  setAllToggles(box => Math.random() < 0.05);
+  toggles.forEach(box => {
+    box.checked = Math.random() < 0.05;
+  });
+  updateVisibility(null);
 });
+
 toggles.forEach(box => {
   box.addEventListener("change", event => {
-    updateVisibility();
+    updateVisibility(event.target);
   });
 });
 
 requestAnimationFrame(timestamp => {
-  initToggles();
-  updateVisibility();
+  initialToggle.indeterminate = true;
   if (getComputedStyle(document.body).display == "flex") {
     document.getElementById("st").open = true;
   }
-  document.getElementById("facade").remove();
 });
